5fcec19573d3c2db5f0567ab29edb576a2f86051
Joshua Campbell <jacampb@bgsu.edu>
2018-04-05 15:19:07 -0700

    Refactored to use the same MySQL library as the other script and to use parameters to take in db connection info to avoid storing credentials in a file cleartext

--- Python/eslinthubdata.py
+++ Python/eslinthubdata.py
@@ -1,16 +1,35 @@
-import MySQLdb
+import mysql.connector
+import argparse
 import sys
-import ESLint
+#import ESLint
 
+#Parse aguments
+parser = argparse.ArgumentParser(description='Queries database for unprocessed repositories, then one at a time will pull then down and lint them with ESLint. Results stored in db.')
+
+parser.add_argument('-u','--user', help='username for the mysql database login',required=True)
+parser.add_argument('-p', '--password', help='password for the mysql database login', required=True)
+parser.add_argument('-s', '--server', help='the server that contains the database(defaults to localhost)',dest='server', default='localhost')
+parser.add_argument('-db' '--databasename', help='database name to connect to(defaults to eslinthub)', dest='dbname', default='eslinthub') 
+args = parser.parse_args()
+
+config={
+	'user':str(args.user),
+	'password':str(args.password),
+	'host':str(args.server),
+	'db':str(args.dbname),
+	}
 #creates the connection, username and password right now are just placeholders
-connection = mySQLdb.connect (host = "127.0.0.1", user = "root", passwd = "password", db = "eslinthub")
+connection = mysql.connector.connect (**config)
+
 #adds cursor object
-cursor = connection.cursor ()
+cursor = connection.cursor()
 
 #exectues the SQL query on ut_repos
-cursor.execute ("select * from ut_repos")
+cursor.execute ("SELECT * FROM ut_repos WHERE coalesce(ESLint, '') <> 'Y'")
+
 #fetch all rows from teh query
-data = cursor.fetchall ()
+data = cursor.fetchall()
+
 #print the rows
 #close the connection
 connection.close()
