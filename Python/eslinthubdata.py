effa1b66c01d28483789113c2716e6e668ddbaf7
Joshua Campbell <jacampb@bgsu.edu>
2018-04-05 15:35:02 -0700

    Added comments/pseudo code to outline general idea for development. Feel free to find a better way of doing it.

--- Python/eslinthubdata.py
+++ Python/eslinthubdata.py
@@ -33,8 +33,31 @@ data = cursor.fetchall()
 #print the rows
 #close the connection
 connection.close()
-for row in data :
+for row in data:
+    #row[0]=repo_id
+    #row[1]=repo_name
+    #row[2]=html_url
+    #row[3]=ESLint
+    #row[4]=insert_dttm
+    #row[5]=last_modified
     print row[0], row[1], row[2], row[3], row[4], row[5]
+
+    #clone row[2] to working directory
+
+    #create list of all .js files
+
+    #for file in files:
+        #ESlint -o lintresults.out filename.js
+        
+        #Wait for process to complete, then parse lintresults.out
+
+        #store results in ut_eslint_issues one record per issue.
+
+    #clear out working directory and process next row/repo
+
+
+
+
 #close the cursor
 cursor.close()
 #close the program
